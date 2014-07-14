13eeb72f5670021dafb4f663d00c5e24807fa3a3
noili <federico.neuman@gmail.com>
2014-07-14 19:53:45 -0300

    Corrected deuda_tgi.

--- deuda_tgi.sql
+++ deuda_tgi.sql
@@ -1,4 +1,5 @@
 select
+  d.cuenta as cuenta,
   t.catastral as 'Numero partida municipal', 
   '01' as tributo,
   '1' as numero_de_recibo, 
@@ -7,11 +8,11 @@ select
   DATE_SUB(p.vencimiento1,INTERVAL 10 DAY) as fecha_emision, 
   p.vencimiento1 as 'Fecha 1er vencimiento', 
   p.vencimiento2 as 'Fecha 2do vencimiento',
-  round(municipal.calcular_interes(d.f_pago, p.vencimiento1,d.avaluo),2) as 'interes resarcitorio',
-  /*IF(d.f_pago is not null, 
+  -- round(municipal.calcular_interes(d.f_pago, p.vencimiento1,d.avaluo),2) as 'interes resarcitorio',
+  IF(d.f_pago is not null, 
     municipal.dreal(d.f_pago, p.vencimiento1, d.avaluo, 0, 0), 
     municipal.dreal(NOW(), p.vencimiento1, d.avaluo, 0, 0))- 
-  municipal.dreal(DATE_ADD(p.vencimiento1,INTERVAL 1 DAY), p.vencimiento1, d.avaluo, 0, 0) as 'interes resarcitorio',*/
+  municipal.dreal(DATE_ADD(p.vencimiento1,INTERVAL 1 DAY), p.vencimiento1, d.avaluo, 0, 0) as 'interes resarcitorio',
   '' as referencia, 
    
   dir.calle as calle,
@@ -31,7 +32,7 @@ select
   con.inicio as 'fecha convenio destino',
 
   d.judicial as 'nro titulo ejecutorio',
-  /*j.fecha as 'fecha titulo ejecutorio',*/
+  j.fecha as 'fecha titulo ejecutorio',
    
   
   '' as NORANUL, 
@@ -57,4 +58,5 @@ from municipal.deuda_tgi d
   left join municipal.ptgi p on d.periodo = p.periodo
   left join municipal.direccion_correo_tgi dir on dir.cod = d.cuenta
   left join municipal.convenio con on con.cod = d.convenio
-  /*left join municipal.judicial j on d.judicial = j.cod*/
+  left join municipal.judicial j on d.judicial = j.cod
+-- where d.cuenta = 20 and d.periodo = 201306
