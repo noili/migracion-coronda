7ddc123ce4da25a503437c02cf029319321ab2b7
noili <federico.neuman@gmail.com>
2014-07-23 17:12:02 -0300

    Reemplazado NULL por ''.

--- deuda_tgi.sql
+++ deuda_tgi.sql
@@ -1,41 +1,42 @@
 select
-  d.cuenta as cuenta,
-  t.catastral as 'Numero partida municipal', 
+  -- d.cuenta as cuenta,
+  if(t.catastral,t.catastral,'') as 'Numero partida municipal', 
   '01' as tributo,
   '1' as numero_de_recibo, 
   municipal.estado(d.pago,d.convenio,d.judicial,d.borrada,d.cancelada) as estado, 
-  d.periodo as periodo, 
+  if(d.periodo,d.periodo,'') as periodo,
   DATE_SUB(p.vencimiento1,INTERVAL 10 DAY) as fecha_emision, 
   p.vencimiento1 as 'Fecha 1er vencimiento', 
   p.vencimiento2 as 'Fecha 2do vencimiento',
-  -- round(municipal.calcular_interes(d.f_pago, p.vencimiento1,d.avaluo),2) as 'interes resarcitorio',
-  IF(d.f_pago is not null, 
+  round(municipal.calcular_interes(d.f_pago, p.vencimiento1,d.avaluo),2) as 'interes resarcitorio',
+  /*IF(d.f_pago is not null, 
     municipal.dreal(d.f_pago, p.vencimiento1, d.avaluo, 0, 0), 
     municipal.dreal(NOW(), p.vencimiento1, d.avaluo, 0, 0))- 
-  municipal.dreal(DATE_ADD(p.vencimiento1,INTERVAL 1 DAY), p.vencimiento1, d.avaluo, 0, 0) as 'interes resarcitorio',
+  municipal.dreal(DATE_ADD(p.vencimiento1,INTERVAL 1 DAY), p.vencimiento1, d.avaluo, 0, 0) as 'interes resarcitorio',*/
   '' as referencia, 
    
-  dir.calle as calle,
-  dir.altura as altura,
-  dir.piso as piso,
-  dir.dpto as dpto, 
-  dir.localidad as localidad,
+  -- dir.calle as calle,
+  if(c.name,c.name,'') as calle,
+  if(dir.altura,dir.altura,'') as altura,
+  if(dir.piso,dir.piso,'') as piso,
+  if(dir.dpto,dir.dpto,'') as dpto, 
+  if(dir.localidad,dir.localidad,'') as localidad,
   
   '' as 'nro convenio origen',
   '' as 'descripcion convenio origen', 
   '' as 'nombre del tipo de convenio origen', 
   '' as 'fecha convenio origen',
 
-  d.convenio as 'nro convenio destino',
-  ' ' as 'descripcion convenio destino', 
+  if(d.convenio,d.convenio,'') as 'nro convenio destino',
+  '' as 'descripcion convenio destino', 
   'convenio' as 'nombre del tipo de convenio destino', 
-  con.inicio as 'fecha convenio destino',
+  if(con.inicio,con.inicio,'') as 'fecha convenio destino',
 
-  d.judicial as 'nro titulo ejecutorio',
-  j.fecha as 'fecha titulo ejecutorio',
+  if(d.judicial,d.judicial,'') as 'nro titulo ejecutorio',
+  if(j.fecha,j.fecha,'') as 'fecha titulo ejecutorio',
    
   
-  '' as NORANUL, 
+  if(d.borrada,d.borrada,'') as NORANUL, 
   '' as TIPOANUL, 
   '' as FECHANUL,
   'tasa pura',
@@ -56,7 +57,9 @@ select
 from municipal.deuda_tgi d
   left join municipal.tgi t on d.cuenta = t.cod
   left join municipal.ptgi p on d.periodo = p.periodo
-  left join municipal.direccion_correo_tgi dir on dir.cod = d.cuenta
-  left join municipal.convenio con on con.cod = d.convenio
+  left join municipal.direccion_correo_tgi dir on d.cuenta = dir.cod
+  left join municipal.convenio con on d.convenio = con.cod 
   left join municipal.judicial j on d.judicial = j.cod
--- where d.cuenta = 20 and d.periodo = 201306
+  left join municipal.calles c on dir.calle = c.id
+-- where dir.dpto = 'null'
+limit 100
