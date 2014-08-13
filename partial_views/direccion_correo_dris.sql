d7438f7b4a03a22172e81f64d9a87f38cfcf1abf
noili <federico.neuman@gmail.com>
2014-08-13 11:31:51 -0300

    Corrected partial views.

--- partial_views/direccion_correo_dris.sql
+++ partial_views/direccion_correo_dris.sql
@@ -1,13 +1,15 @@
-drop view municipal.direccion_correo_dris;
-
-create view municipal.direccion_correo_dris as
-select
-  t.id, t.calle_id as calle, t.altura as altura , '  ' as piso, t.dpto as dpto, l.name as localidad
-  -- IF(cod_correo = 0,t.calle,IF(cod_correo = 1,c1.calle,c2.calle)) as calle,
-  -- IF(cod_correo = 0,t.altura,IF(cod_correo = 1,c1.altura,c2.altura)) as altura,
-  -- IF(cod_correo = 0,' ',IF(cod_correo = 1,c1.piso,c2.piso)) as piso,
-  -- IF(cod_correo = 0,t.dpto,IF(cod_correo = 1,c1.dpto,c2.dpto)) as dpto,
-  -- IF(cod_correo = 0,l.name,IF(cod_correo = 1,c1.localidad,c2.localidad)) as localidad
-from municipal.dris t
-  -- left join municipal.direcciones_contribuyentes c1 on t.contribuyente_id = c1.id
-  left join municipal.localidads l on t.localidad_id = l.id
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `direccion_correo_dris` AS
+    select 
+        `t`.`id` AS `id`,
+        `t`.`calle_id` AS `calle`,
+        `t`.`altura` AS `altura`,
+        '  ' AS `piso`,
+        `t`.`dpto` AS `dpto`,
+        `l`.`name` AS `localidad`
+    from
+        (`dris` `t`
+        left join `localidads` `l` ON ((`t`.`localidad_id` = `l`.`id`)))
--- partial_views/direccion_correo_tgi.sql
+++ partial_views/direccion_correo_tgi.sql
@@ -1,15 +1,45 @@
-drop view municipal.direccion_correo_tgi;
-
-create view municipal.direccion_correo_tgi as
-select
-  t.cod,
-  IF(cod_inm = 0,t.calle,IF(cod_inm = 1,c1.calle,c2.calle)) as calle,
-  IF(cod_inm = 0,t.altura,IF(cod_inm = 1,c1.altura,c2.altura)) as altura,
-  IF(cod_inm = 0,' ',IF(cod_inm = 1,c1.piso,c2.piso)) as piso,
-  IF(cod_inm = 0,t.dpto,IF(cod_inm = 1,c1.dpto,c2.dpto)) as dpto,
-  IF(cod_inm = 0,l.name,IF(cod_inm = 1,c1.localidad,c2.localidad)) as localidad,
-  t.cate1, t.cate2, t.cate3, t.cate4, municipal.mi_max(t.cate1, t.cate2, t.cate3, t.cate4) as categoria
-from municipal.tgi t
-  left join municipal.direcciones_contribuyentes c1 on t.contri = c1.id
-  left join municipal.direcciones_contribuyentes c2 on t.contri2 = c2.id
-  left join municipal.localidads l on t.localidad = l.id
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `direccion_correo_tgi` AS
+    select 
+        `t`.`cod` AS `cod`,
+        if((`t`.`cod_inm` = 0),
+            `t`.`calle`,
+            if((`t`.`cod_inm` = 1),
+                `c1`.`calle`,
+                `c2`.`calle`)) AS `calle`,
+        if((`t`.`cod_inm` = 0),
+            convert( `t`.`altura` using utf8),
+            if((`t`.`cod_inm` = 1),
+                `c1`.`altura`,
+                `c2`.`altura`)) AS `altura`,
+        if((`t`.`cod_inm` = 0),
+            ' ',
+            if((`t`.`cod_inm` = 1),
+                `c1`.`piso`,
+                `c2`.`piso`)) AS `piso`,
+        if((`t`.`cod_inm` = 0),
+            convert( `t`.`dpto` using utf8),
+            if((`t`.`cod_inm` = 1),
+                `c1`.`dpto`,
+                `c2`.`dpto`)) AS `dpto`,
+        if((`t`.`cod_inm` = 0),
+            `l`.`name`,
+            if((`t`.`cod_inm` = 1),
+                `c1`.`localidad`,
+                `c2`.`localidad`)) AS `localidad`,
+        `t`.`cate1` AS `cate1`,
+        `t`.`cate2` AS `cate2`,
+        `t`.`cate3` AS `cate3`,
+        `t`.`cate4` AS `cate4`,
+        `municipal`. `mi_max` (`t`.`cate1`,
+                `t`.`cate2`,
+                `t`.`cate3`,
+                `t`.`cate4`) AS `categoria`
+    from
+        (((`tgi` `t`
+        left join `direcciones_contribuyentes` `c1` ON ((`t`.`contri` = `c1`.`id`)))
+        left join `direcciones_contribuyentes` `c2` ON ((`t`.`contri2` = `c2`.`id`)))
+        left join `localidads` `l` ON ((`t`.`localidad` = `l`.`id`)))
