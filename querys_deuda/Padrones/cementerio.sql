317566618fa3f2a1d6d59f5338bd591b5d993a4b
noili <federico.neuman@gmail.com>
2015-07-30 18:59:32 -0300

    Cementerio.

--- querys_deuda/Padrones/cementerio.sql
+++ querys_deuda/Padrones/cementerio.sql
@@ -1,6 +1,6 @@
-select 
+select
   c.cod as 'numero partida municipal',
-  '3-cementerio' as 'tipo',
+  '&7' as 'tributo',
   c.superficie as 'superficie',
   1 as 'tipo de documento',
   if(c.contri,contri.nro_doc,if(c.contri2,contri2.nro_doc,'')) as 'nro doc',
@@ -8,14 +8,14 @@ select
   if(c.contri,contri.name,if(c.contri2,contri2.name,'')) as 'nombre',
   if(c.contri,contri.telefono,if(c.contri2,contri2.telefono,'')) as 'telefono',
   '' as 'sexo',
-  if(c.contri,contri.email,if(c.contri2,contri2.email,'')) as 'email',
-  if(c.contri,dir.nombre,if(c.contri2,dir2.nombre,'')) as 'calle',
+  -- if(c.contri,contri.email,if(c.contri2,contri2.email,'')) as 'email',
+  if(c.contri,cal.name,if(c.contri2,cal2.name,'')) as 'calle',
   if(c.contri,dir.altura,if(c.contri2,dir2.altura,'')) as 'altura',
   if(c.contri,dir.piso,if(c.contri2,dir2.piso,'')) as 'piso',
   if(c.contri,dir.dpto,if(c.contri2,dir2.dpto,'')) as 'dpto',
   if(c.contri,dir.localidad,if(c.contri2,dir2.localidad,'')) as 'localidad',
   c.difunto as 'difunto',
-  if('nicho','&3','&2') as 'nicho',
+  if(c.superficie = 0 or c.superficie = NULL,'&2','&3') as 'nicho',
   concat(c.carac, ' - ', c.fila, ' - ', c.nro, ' - ', c.seccion) as 'nota',
   if(c.baja != 1, 'A', 'N')as 'estado'
 
@@ -24,4 +24,5 @@ from municipal.cementerio c
   left join municipal.contribuyentes contri2 on c.contri2 = contri2.id
   left join municipal.direcciones_contribuyentes dir on c.contri = dir.id
   left join municipal.direcciones_contribuyentes dir2 on c.contri2 = dir2.id
-  
+  left join municipal.calles cal on contri.calle_id = cal.id
+  left join municipal.calles cal2 on contri2.calle_id = cal2.id
